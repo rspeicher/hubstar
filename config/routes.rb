e9491b6924ed0a8a839ed2cf44f6231d76a7b1eb
Robert Speicher <rspeicher@gmail.com>
2012-02-28 18:35:27 -0500

    Add an 'About' page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Hubstar::Application.routes.draw do
   get 'sign_in', :to => 'sessions#new', :as => :sign_in
   delete 'sign_out', :to => 'sessions#destroy', :as => :sign_out
 
+  get 'about', :to => 'dashboard#about', :as => :about
+
   resources :repositories, :only => [:index, :show, :update, :destroy], :constraints => {:id => /[\w\-\+\/]+/}
   resource :star, :only => [:update, :destroy]
 
