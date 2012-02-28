b2c101d0d59675b49c60c90ee8e99ac3ba4197cb
Robert Speicher <rspeicher@gmail.com>
2012-02-28 18:17:38 -0500

    Load config/heroku_env.rb if present
    Stores GitHub app keys for development

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+heroku_env = File.expand_path('../heroku_env.rb', __FILE__)
+load(heroku_env) if File.exists?(heroku_env)
+
 # Initialize the rails application
 Hubstar::Application.initialize!
