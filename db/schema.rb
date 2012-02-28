15a97b5a790f069759819d246a1969abf715f1f2
Robert Speicher <rspeicher@gmail.com>
2012-02-28 02:24:39 -0500

    Change Repository description field to TEXT

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120128220559) do
+ActiveRecord::Schema.define(:version => 20120228071334) do
 
   create_table "repositories", :force => true do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.integer  "watchers"
     t.integer  "forks"
     t.datetime "created_at",  :null => false
