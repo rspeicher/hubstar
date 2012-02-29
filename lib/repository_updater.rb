f0ca49b5984028431e2ecf715e99f48d8e9880df
Robert Speicher <rspeicher@gmail.com>
2012-02-29 14:32:32 -0500

    RepositoryUpdater changes
    - Updates updated_at for each repo no matter what
    - Expand limit to 15

--- lib/repository_updater.rb
+++ lib/repository_updater.rb
@@ -3,14 +3,21 @@ require 'octokit'
 class RepositoryUpdater
   class << self
     def update
-      Repository.order('updated_at').limit(10).each do |repo|
+      Repository.order('updated_at ASC').limit(15).each do |repo|
         begin
           i = Octokit.repository(repo.name)
           puts "--> #{repo.to_s}"
-          repo.update_attributes(forks:       i.forks,
-                                 watchers:    i.watchers,
-                                 description: i.description,
-                                 language:    i.language)
+
+          # Manually setting updated_at so that even if none of the other
+          # attributes have changed, we won't crawl this repo again immediately
+          repo.update_attributes({
+            forks:       i.forks,
+            watchers:    i.watchers,
+            description: i.description,
+            language:    i.language,
+            updated_at:  Time.now
+          }, without_protection: true)
+
         rescue Octokit::NotFound => e
           # TODO: Maybe have an error_count field, delete repos that fail too many updates
           puts "    #{e}"
