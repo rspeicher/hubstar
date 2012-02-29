03719642708ff3044305d535b7cbfd5fc23be399
Robert Speicher <rspeicher@gmail.com>
2012-02-29 14:50:04 -0500

    Do repository updates less stupidly
    I was trying to avoid an extra query, but whatever.

--- lib/repository_updater.rb
+++ lib/repository_updater.rb
@@ -5,18 +5,16 @@ class RepositoryUpdater
     def update
       Repository.order('updated_at ASC').limit(15).each do |repo|
         begin
-          i = Octokit.repository(repo.name)
           puts "--> #{repo.to_s}"
+          repo.touch
 
-          # Manually setting updated_at so that even if none of the other
-          # attributes have changed, we won't crawl this repo again immediately
+          i = Octokit.repository(repo.name)
           repo.update_attributes({
             forks:       i.forks,
             watchers:    i.watchers,
             description: i.description,
-            language:    i.language,
-            updated_at:  Time.now
-          }, without_protection: true)
+            language:    i.language
+          })
 
         rescue Octokit::NotFound => e
           # TODO: Maybe have an error_count field, delete repos that fail too many updates
