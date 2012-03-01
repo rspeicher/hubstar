534df8c91dc760061619a704adc5ae7a12933175
Robert Speicher <rspeicher@gmail.com>
2012-03-01 17:48:19 -0500

    Add RepositoryUpdater.bootstrap
    Instead of updating least recently updated repos, updates all repos for which
    we have no data.

--- lib/repository_updater.rb
+++ lib/repository_updater.rb
@@ -2,24 +2,44 @@ require 'octokit'
 
 class RepositoryUpdater
   class << self
-    def update
-      Repository.order('updated_at ASC').limit(15).each do |repo|
-        begin
-          puts "--> #{repo.to_s}"
-          repo.touch
+    # Public: Fetch initial data for a repository
+    def bootstrap
+      Repository.where(watchers: nil, forks: nil).each do |repo|
+        puts "--> #{repo.to_s}"
+        fetch(repo)
+      end
+    end
 
-          i = Octokit.repository(repo.name)
-          repo.update_attributes({
-            forks:       i.forks,
-            watchers:    i.watchers,
-            description: i.description,
-            language:    i.language
-          })
+    # Public: Update least recently updated repositories.
+    #
+    # limit - The number of repositories to update
+    def update(limit = 15)
+      Repository.order('updated_at ASC').limit(limit).each do |repo|
+        puts "--> #{repo.to_s}"
+        fetch(repo)
+      end
+    end
 
-        rescue Octokit::NotFound => e
-          # TODO: Maybe have an error_count field, delete repos that fail too many updates
-          puts "    #{e}"
-        end
+    private
+
+    # Private: Fetch the latest info for a repository from GitHub
+    #
+    # repo - The Repository record to update
+    def fetch(repo)
+      begin
+        repo.touch
+
+        i = Octokit.repository(repo.name)
+        repo.update_attributes({
+          forks:       i.forks,
+          watchers:    i.watchers,
+          description: i.description,
+          language:    i.language
+        })
+
+      rescue Octokit::NotFound => e
+        # TODO: Maybe have an error_count field, delete repos that fail too many updates
+        puts "    #{e}"
       end
     end
   end
